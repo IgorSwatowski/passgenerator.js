b56e8bca695fd51f1756f3cabac8f68ae58d17ff
IgorSwatowski <igi66@wp.pl>
2020-05-24 13:38:01 +0200

    Rewrite rest main code to Generator Component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <Navbar/>
+    <Generator/>
   </div>
 </template>
 
 <script>
 import Navbar from './components/Navbar.vue'
+import Generator from './components/Generator.vue'
 
 export default {
   name: 'App',
   components: {
-    Navbar
+    Navbar,
+    Generator
   }
 }
 </script>
---
+++ src/components/Generator.vue
@@ -0,0 +1,33 @@
+<template>
+   <div class="container-fluid">
+        <h3 class="col-md-6 col-lg-12 text-center">Password Generator</h3>
+            <div class="information col-md-6 col-lg-12">
+            <h3>Instruction: </h3><br>
+            1. Move the slider to increase the password length <br>
+            2. Click generate to generate the password <br>
+            3. The maximum password length can be 30 <br><br><br>
+            </div>
+            <div class="generator col-md-6 col-lg-12 text-center">
+            <input type="text" id="display">
+            <input type="range" min="0" max="30" name="passwordLength" id="passwordLength">
+            <button @click="passwdGenerate()">Generate</button>
+            <div id="length"></div>
+            </div>
+    </div>
+</template>
+
+<script>
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'bootstrap-vue/dist/bootstrap-vue.min.js'
+
+export default {
+    name: 'Generator',
+}
+</script>
+
+<style lang="scss" scoped>
+
+@import './src/scss/Generator/_generator.scss';
+
+</style>
